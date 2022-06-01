49d7b675a5e35a9df569ea4907563ca0647a4634
Chris Le <le.chrisv@gmail.com>
2022-06-01 01:38:03 -0400

    Added UI and score keeping

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Define buttons
+const rockBtn = document.getElementById('rock-btn');
+const paperBtn = document.getElementById('paper-btn');
+const scissorsBtn = document.getElementById('scissors-btn');
+const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const resultsDisplay = document.querySelector('#results > p');
+let playerSelection;
+let computerSelection;
+
+// Initalize scores
+let playerScore = 0;
+let computerScore = 0;
+
+playerScoreDisplay.innerHTML = playerScore;
+computerScoreDisplay.innerHTML = computerScore;
+
 // Randomly generated computer selection
 function computerPlay() {
     let computerArr = ['rock', 'paper', 'scissors'];
@@ -7,85 +24,85 @@ function computerPlay() {
     return computerPick
 }
 
-// Define results
-const playerW = "player wins";
-const compW = "computer wins";
-const tie = "tie";
+// Event listener for each button clicked
+rockBtn.addEventListener('click', (e) => {
+    console.log('clicked');
+    playerSelection = 'rock';
+    playRound(playerSelection)
+});
+paperBtn.addEventListener('click', (e) => {
+    console.log('clicked');
+    playerSelection = 'paper';
+    playRound(playerSelection)
+});
+scissorsBtn.addEventListener('click', (e) => {
+    console.log('clicked');
+    playerSelection = 'scissors';
+    playRound(playerSelection)
+});
+
 
 // Function for one round of RPS
-function playRound ( playerSelection, computerSelection) {
-
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return tie;
-    } else if (playerSelection.toLowerCase() === "rock") {
-        if (computerSelection.toLowerCase() === "scissors") {
-            return playerW;
-        } else if (computerSelection.toLowerCase() === "paper") {
-            return compW;
+function playRound (playerSelection) {
+    computerSelection = computerPlay();
+    console.log('playing');
+    if (playerSelection === computerSelection) {
+        return tie();
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            playerWin();
+        } else if (computerSelection === "paper") {
+            computerWin();
         }
 
-    } else if (playerSelection.toLowerCase() === "paper") {
-        if (computerSelection.toLowerCase() === "rock") {
-            return playerW;
-        } else if (computerSelection.toLowerCase() === "scissors") {
-            return compW;
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            playerWin();
+        } else if (computerSelection === "scissors") {
+            computerWin();
         }
 
-    } else if (playerSelection.toLowerCase() === "scissors") {
-        if (computerSelection.toLowerCase() === "paper") {
-            return playerW;
-        } else if (computerSelection.toLowerCase() === "rock") {
-            return compW;
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            playerWin();
+        } else if (computerSelection === "rock") {
+            computerWin();
         }
-    }
-}
-
-// Function for one game of 5 rounds of RPS
-function game () {
-
-  console.log('Starting rock, paper, scissors...');
-  console.log('Best of 5 rounds.')
-
-    // Initalize scores
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // Loop through 5 rounds
-    for (var i = 0; i < 5; i++) {
-
-        // Prompt player for selections
-        let playerPrompt = prompt("Select rock, paper or scissors!");
-        let playerSelection = playerPrompt.toLowerCase();
-        let computerSelection = computerPlay();
-
-        // Store result of round in variable
-        let result = playRound(playerSelection, computerSelection);
-
-        // Print result of each round
-        console.log('Round ' + i);
-        if (result === playerW) {
-            playerScore++;
-            console.log(playerW);
-        } else if (result === compW) {
-            computerScore++;
-            console.log(compW);
-        } else if (result === tie) {
-            console.log(tie);
-        }
-    }
-
-    // Print final scores for player and computer
-    console.log(playerScore);
-    console.log(computerScore);
-
-    // Calculate winner
-    if (playerScore > computerScore) {
-        return "Player wins!";
-    } else if (playerScore < computerScore) {
-        return "Computer wins!";
     } else {
-        return "It's a tie!";
+        console.log('error')
     }
 }
 
-console.log(game());
+// Define results
+function tie() {
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore;
+    resultsDisplay.innerHTML = "It's a tie!";
+    console.log('tie')
+}
+
+function playerWin() {
+    playerScore++;
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore;
+    console.log('player win')
+
+    if (playerScore < 5) {
+        resultsDisplay.innerHTML = playerSelection + " beats " + computerSelection +"!";
+    } else if (playerScore === 5) {
+        resultsDisplay.innerHTML = "Game over! You WIN!"
+    }
+}
+
+function computerWin() {
+    computerScore++
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore;
+    console.log('computer win')
+
+    if (computerScore < 5) {
+        resultsDisplay.innerHTML = computerSelection + " beats " + playerSelection +"!";
+    } else if (computerScore === 5) {
+        resultsDisplay.innerHTML = "Game over! You LOSE!"
+    }
+}
