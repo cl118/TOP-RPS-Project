5f1dbae66805379abc5aeaea7da1b20ca6e596a6
Chris Le <le.chrisv@gmail.com>
2022-06-01 02:16:46 -0400

    Buttons disabled when game over. New game button added to reset scores and activate buttons.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const scissorsBtn = document.getElementById('scissors-btn');
 const playerScoreDisplay = document.getElementById('player-score');
 const computerScoreDisplay = document.getElementById('computer-score');
 const resultsDisplay = document.querySelector('#results > p');
+const resetDisplay = document.querySelector('#reset');
+
 let playerSelection;
 let computerSelection;
 
@@ -17,7 +19,7 @@ computerScoreDisplay.innerHTML = computerScore;
 
 // Randomly generated computer selection
 function computerPlay() {
-    let computerArr = ['rock', 'paper', 'scissors'];
+    let computerArr = ['Rock', 'Paper', 'Scissors'];
     let randomNum = Math.floor(Math.random()*3);
     let computerPick = computerArr[randomNum];
 
@@ -27,17 +29,17 @@ function computerPlay() {
 // Event listener for each button clicked
 rockBtn.addEventListener('click', (e) => {
     console.log('clicked');
-    playerSelection = 'rock';
+    playerSelection = 'Rock';
     playRound(playerSelection)
 });
 paperBtn.addEventListener('click', (e) => {
     console.log('clicked');
-    playerSelection = 'paper';
+    playerSelection = 'Paper';
     playRound(playerSelection)
 });
 scissorsBtn.addEventListener('click', (e) => {
     console.log('clicked');
-    playerSelection = 'scissors';
+    playerSelection = 'Scissors';
     playRound(playerSelection)
 });
 
@@ -48,28 +50,26 @@ function playRound (playerSelection) {
     console.log('playing');
     if (playerSelection === computerSelection) {
         return tie();
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") {
             playerWin();
-        } else if (computerSelection === "paper") {
+        } else if (computerSelection === "Paper") {
             computerWin();
         }
 
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
             playerWin();
-        } else if (computerSelection === "scissors") {
+        } else if (computerSelection === "Scissors") {
             computerWin();
         }
 
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") {
             playerWin();
-        } else if (computerSelection === "rock") {
+        } else if (computerSelection === "Rock") {
             computerWin();
         }
-    } else {
-        console.log('error')
     }
 }
 
@@ -90,7 +90,8 @@ function playerWin() {
     if (playerScore < 5) {
         resultsDisplay.innerHTML = playerSelection + " beats " + computerSelection +"!";
     } else if (playerScore === 5) {
-        resultsDisplay.innerHTML = "Game over! You WIN!"
+        resultsDisplay.innerHTML = "Game over! You WIN!";
+        gameOver();
     }
 }
 
@@ -98,11 +99,45 @@ function computerWin() {
     computerScore++
     playerScoreDisplay.innerHTML = playerScore;
     computerScoreDisplay.innerHTML = computerScore;
-    console.log('computer win')
+    console.log('computer win');
 
     if (computerScore < 5) {
         resultsDisplay.innerHTML = computerSelection + " beats " + playerSelection +"!";
     } else if (computerScore === 5) {
         resultsDisplay.innerHTML = "Game over! You LOSE!"
+        gameOver();
     }
+}
+
+function gameOver() {
+    rockBtn.setAttribute('disabled', 1);
+    paperBtn.setAttribute('disabled', 1);
+    scissorsBtn.setAttribute('disabled', 1);
+    createResetBtn();
+
+}
+
+function createResetBtn() {
+    let btn = document.createElement('button');
+    btn.id = 'reset-btn';
+    resetDisplay.appendChild(btn);
+    let resetBtn = document.querySelector('#reset > button')
+    resetBtn.innerHTML = 'NEW GAME';
+    resetBtn.addEventListener('click', (e) => resetGame());
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+
+    playerScoreDisplay.innerHTML = playerScore;
+    computerScoreDisplay.innerHTML = computerScore;
+
+    resultsDisplay.innerHTML = '';
+
+    resetDisplay.removeChild(document.getElementById('reset-btn'));
+
+    rockBtn.removeAttribute('disabled');
+    paperBtn.removeAttribute('disabled');
+    scissorsBtn.removeAttribute('disabled');
 }
